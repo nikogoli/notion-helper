14163b9fd62b475691281c295f893b1240f9fd39
unknown <k_m_naoto@yahoo.co.jp>
2022-04-29 16:03:04 +0900

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,24 +1,9 @@
-/*
-import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
-
-function handler(req: Request): Response {
-  console.log(req)
-  return new Response("Hello world");
-}
-
-console.log("Listening on http://localhost:8000");
-await serve(handler);
-*/
-
-
 import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
 import { ConnInfo } from "https://deno.land/std/http/server.ts";
 
 import { Client} from "https://deno.land/x/notion_sdk/src/mod.ts"
 import {
     BlockObjectRequest,
-    CreatePageBodyParameters,
-    RichTextItemRequest,
 } from "https://deno.land/x/notion_sdk/src/api-endpoints.ts"
 
 import { 
@@ -28,24 +13,9 @@ import {
  } from "https://pax.deno.dev/nikogoli/notion-helper/mod.ts"
 
 
-type BlockInfo = {
-    self_id: string,
-    notion_id: string,
-    block: BlockObjectRequest,
-    thread_idx: string,
-    scrap_idx: string,
-    parent_id: null | string,
-}
-
-
-type BlocksFromZenn = {
-    properties: { title: Required<RichTextItemRequest>[] },
-    icon: CreatePageBodyParameters["icon"],
-    children: BlockObjectRequest[];
-}
-
 
 type RequestJson = {
+    url: string,
     html_doc: string,
     target_id: string,
 }
@@ -59,32 +29,12 @@ const HEADER_OPS = {
 }
 
 
-async function data_to_page(
-  	request: Request,
-	
-){
-    const headers = new Headers(HEADER_OPS)
-
-    const auth_head = request.headers.get("Authorization")
-    if ( auth_head !== null ){
-        const token = auth_head.split(" ")[1]
-        if (token != Deno.env.get("USER_TOKEN")){
-            return  new Response("", {headers, "status" : 401 , "statusText" : "Unauthorized" })
-        }
-    }
-	
-	const request_json: RequestJson = await request.json()
-  	const { html_doc, target_id } = request_json
-
-    const orig_url = request.headers.get("origin")
-    if (orig_url === null || !orig_url.includes("articles") || !orig_url.includes("scrap")){
-        throw new Error(JSON.stringify(request.headers))
-    }
-
-    const { title, author, topics, icon, max, topblock_ids, children_ids, data } = (orig_url.includes("articles"))
-        ? await article_to_blocks(orig_url, html_doc)
-        : await scrap_to_blocks(orig_url, html_doc)
-
+async function call_api(
+    headers: Headers,
+    target_id: string,
+    body: ZennResponse,
+) {
+    const { title, author, topics, icon, max, topblock_ids, children_ids, data } = body
     const children = topblock_ids.map(id => data[id].block)
     const properties = {title}
 
@@ -96,11 +46,11 @@ async function data_to_page(
         children: children,
     })
     .catch((e) =>{
-        return new Response(JSON.stringify(e), {headers, status:400})
+        return { ok: false, data: JSON.stringify(e), status: 400 }
     })
 
     if (children_ids.length == 0){
-        return new Response(JSON.stringify(notion_response), {headers, status:200})
+        return { ok: true, data: JSON.stringify(notion_response), status: 200 }
     }
 
     const id_and_childs = children_ids.reduce( (dict, id) =>{
@@ -126,28 +76,45 @@ async function data_to_page(
     }, Promise.resolve())
 
     if ([...Object.keys(failed_logs)].length > 0){
-        return new Response(JSON.stringify({...notion_response, logs:failed_logs}), {headers, status:600})
+        return { ok: false, data: JSON.stringify({...notion_response, logs:failed_logs}), status: 600}
+    } else {
+        return { ok: true, data: JSON.stringify(notion_response), status: 200}
     }
-    return new Response(JSON.stringify(notion_response), {headers, status:200})
 }
 
 
-async function handle_rooted_req(
-    request: Request,
-    _connInfo: ConnInfo,
-    params: {target: string, method: string}
-) {
-    if (request.method == "OPTIONS"){
-        return new Response("options", {headers: HEADER_OPS, status: 200})
+
+async function data_to_page(
+  	request: Request,
+	
+){
+    const headers = new Headers(HEADER_OPS)
+
+    const auth_head = request.headers.get("Authorization")
+    if (auth_head === null) {
+        return new Response("", {headers, "status" : 401 , "statusText" : "Unauthorized" })
     }
-    const { target, method } = params
-    if (target == "pages"){
-        if (method == "create"){
-            return await data_to_page(request)
-        }
-    } else {
+
+    const is_valid = (auth_head.split(" ")[1] == Deno.env.get("USER_TOKEN"))
+    if (is_valid == false ){
+        return  new Response("", {headers, "status" : 401 , "statusText" : "Unauthorized" })
+    }
+
+	const request_json: RequestJson = await request.json()
+  	const { url, html_doc, target_id } = request_json
+
+    if (!url.includes("articles") && !url.includes("scrap")){
         return new Response("not implemented", {headers: HEADER_OPS, status: 501})
     }
+
+    const convertion_result =  (url.includes("articles")) ? await article_to_blocks(url, html_doc) : await scrap_to_blocks(url, html_doc)
+    if (convertion_result.ok == false){
+        return new Response(JSON.stringify(convertion_result.data), {headers, status:400})
+    }
+
+    const { ok, data, status } = await call_api(headers, target_id, convertion_result.data)
+    if (ok == false){ console.log(JSON.parse(data)) }
+    return new Response(data, {headers, status})
 }
 
 
@@ -156,11 +123,23 @@ serve({
         return new Response("Hellow", {headers: HEADER_OPS, status: 200})
     },
 
-    "/withid/:target/:method": (
+    "/withid/:target/:method": async (
         request: Request,
         connInfo: ConnInfo,
         params: {target: string, method: string}
-    ) => handle_rooted_req(request, connInfo, params),
+    ) => {
+        if (request.method == "OPTIONS"){
+            return new Response("options", {headers: HEADER_OPS, status: 200})
+        }
+        const { target, method } = params
+        if (target == "pages"){
+            if (method == "create"){
+                return await data_to_page(request)
+            }
+        } else {
+            return new Response("not implemented", {headers: HEADER_OPS, status: 501})
+        }
+    },
 
     404: (request: Request) => {
         if (request.method == "OPTIONS"){
@@ -169,3 +148,16 @@ serve({
         return new Response("not found", {headers: HEADER_OPS, status: 404})
     },
 })
+
+
+/*
+import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
+
+function handler(req: Request): Response {
+  console.log(req)
+  return new Response("Hello world");
+}
+
+console.log("Listening on http://localhost:8000");
+await serve(handler);
+*/

7d6473cf0ac83408d7af04389ceaa10af967f1e3
unknown <k_m_naoto@yahoo.co.jp>
2022-04-29 21:17:00 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -18,11 +18,14 @@ export {
 } from "./html_to_blocks.ts"
 
 export {
-    article_to_blocks,
-    scrap_to_blocks,
-    zenn_to_blocks,
+    zenn_article_to_blocks,
+    zenn_scrap_to_blocks,
 } from "./functions_zenn.ts"
 
+export { note_article_to_blocks } from "./functions_note.ts"
+
+
+export const TEX_TAGS = ["EMBED-KATEX", "NWC-FORMULA"]
 
 export const VALID_LANGNAME = ["abap", "arduino", "bash", "basic", "c", "clojure", "coffeescript", "c++", "c#", "css", "dart", "diff", "docker", "elixir", "elm", "erlang", "flow", "fortran", "f#", "gherkin", "glsl", "go", "graphql", "groovy", "haskell", "html", "java", "javascript", "json", "julia", "kotlin", "latex", "less", "lisp", "livescript", "lua", "makefile", "markdown", "markup", "matlab", "mermaid", "nix", "objective-c", "ocaml", "pascal", "perl", "php", "plain text", "powershell", "prolog", "protobuf", "python", "r", "reason", "ruby", "rust", "sass", "scala", "scheme", "scss", "shell", "solidity", "sql", "swift", "typescript", "vb.net", "verilog", "vhdl", "visual basic", "webassembly", "xml", "yaml", "java/c/c++/c#"]
 

2ad711e4bcf8ea87547d4f68767be83986f5d156
unknown <k_m_naoto@yahoo.co.jp>
2022-04-29 12:36:53 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -2,6 +2,9 @@ export {
     article_to_blocks,
     scrap_to_blocks,
     zenn_to_blocks,
+} from "./html_to_blocks.ts"
+
+export type {
     BlockInfo,
     ZennResponse,
 } from "./html_to_blocks.ts"
